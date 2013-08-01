f582cfaf94c8c3f19f88949b3884d49f104bb04e
Geraldo Augusto Massahud Rodrigues dos Santos <massahud@gmail.com>
2013-08-01 01:09:36 -0400

    inicio da utilização com firefox

--- copy-paste-rtc/copy-paste-client.js
+++ copy-paste-rtc/copy-paste-client.js
@@ -9,15 +9,35 @@
  * guided by console instructions.
  *
  */
- 
- console.log("CLIENT");
- console.log("FOLLOW THE CONSOLE STEPS TO OPEN CONNECTION");
- 
- 
- var pc = new RTCPeerConnection({"iceServers": [{"url": "stun:stun.l.google.com:19302"}]}, {optional: [{RtpDataChannels: true}]});
- pc.onicecandidate = onIceCandidate;
- 
- var mediaConstraints = {
+
+console.log("CLIENT");
+console.log("FOLLOW THE CONSOLE STEPS TO OPEN CONNECTION");
+
+
+var options = {
+    optional: [{
+        RtpDataChannels: true
+    }, {
+        DtlsSrtpKeyAgreement: true
+    }]
+};
+var iceServers;
+if (webrtcDetectedBrowser == "chrome") {
+    iceServers = {
+        "iceServers": createIceServer("stun:stun.l.google.com:19302")
+    };
+}
+else if (webrtcDetectedBrowser == "firefox") {
+    iceServers = {
+        "iceServers": createIceServer("stun:stun.services.mozilla.com")
+    };
+}
+
+
+var pc = new RTCPeerConnection(iceServers, options);
+pc.onicecandidate = onIceCandidate;
+
+var mediaConstraints = {
     optional: [],
     mandatory: {
         OfferToReceiveAudio: false, // Hmm!!
@@ -25,60 +45,64 @@
     }
 };
 createChannel();
- pc.createOffer(onDescription, null, mediaConstraints);
- 
- var channel;
- 
- 
- 
- var iceCandidates = [];
- function onIceCandidate(event) {
-     //console.log("onIceCandidate");
-     if (event.candidate) {
+
+pc.createOffer(onDescription, null, mediaConstraints);
+
+var channel;
+
+
+
+var iceCandidates = [];
+
+function onIceCandidate(event) {
+    //console.log("onIceCandidate");
+    if (event.candidate) {
         iceCandidates.push(event.candidate);
-     }
- }
- 
- function createChannel() {
-     channel= pc.createDataChannel('RTCDataChannel',{reliable:false});
-     channel.onmessage = onMessage;
-     channel.onopen = onChannelStateChange;
-     channel.onclose = onChannelStateChange;
-     
- }
- 
- 
- 
- function setCandidates(candidates) {
-     for (var i = 0; i < candidates.length; i++) {
+    }
+}
+
+function createChannel() {
+    channel = pc.createDataChannel('RTCDataChannel', {
+        reliable: false
+    });
+    channel.onmessage = onMessage;
+    channel.onopen = onChannelStateChange;
+    channel.onclose = onChannelStateChange;
+
+}
+
+
+
+function setCandidates(candidates) {
+    for (var i = 0; i < candidates.length; i++) {
         pc.addIceCandidate(new RTCIceCandidate(candidates[i]));
-     }     
- }
- 
- function onChannelStateChange(event) {
-     if (event.srcElement.readyState == "open") {
+    }
+}
+
+function onChannelStateChange(event) {
+    if (event.srcElement.readyState == "open") {
         console.log("CONNECTION ESTABLISHED: now use channel.send('message') to send messages");
-     }
- }
- 
- function onDescription(desc) {
-     console.log("1 - Offer created. Send offer to server. Copy & Paste on the server console:");
-     pc.setLocalDescription(desc);
-     console.log('receiveOffer(new RTCSessionDescription(JSON.parse(\'' + JSON.stringify(desc).replace(/\\/g,"\\\\") + '\')));')
- }
- 
- function setRemoteDescription(desc) {
-     pc.setRemoteDescription(desc);
-     console.log("3 - SEND ice candidates to server. Copy & Paste on the server console:");
-     console.log("setCandidates(JSON.parse('" + JSON.stringify(iceCandidates).replace(/\\/g,"\\\\") + "'));")
- }
- 
- function close() {
-     console.log("close");
-     channel.close();
-     pc.close();     
- }
- 
- function onMessage(event) {
-     console.log('Message received: ' + event.data);     
- }
+    }
+}
+
+function onDescription(desc) {
+    console.log("1 - Offer created. Send offer to server. Copy & Paste on the server console:");
+    pc.setLocalDescription(desc);
+    console.log('receiveOffer(new RTCSessionDescription(JSON.parse(\'' + JSON.stringify(desc).replace(/\\/g, "\\\\") + '\')));')
+}
+
+function setRemoteDescription(desc) {
+    pc.setRemoteDescription(desc);
+    console.log("3 - SEND ice candidates to server. Copy & Paste on the server console:");
+    console.log("setCandidates(JSON.parse('" + JSON.stringify(iceCandidates).replace(/\\/g, "\\\\") + "'));")
+}
+
+function close() {
+    console.log("close");
+    channel.close();
+    pc.close();
+}
+
+function onMessage(event) {
+    console.log('Message received: ' + event.data);
+}
--- copy-paste-rtc/copy-paste-server.js
+++ copy-paste-rtc/copy-paste-server.js
@@ -9,83 +9,106 @@
  * guided by console instructions.
  *
  */
- 
- 
- console.log("SERVER");
- console.log("THE FIRST STEP IS ON THE CLIENT CONSOLE");
- 
- var pc = new RTCPeerConnection({"iceServers": [{"url": "stun:stun.l.google.com:19302"}]}, {optional: [{RtpDataChannels: true}]});
- pc.onicecandidate = onIceCandidate;
- pc.ondatachannel = onDataChannel;
- var channel;
- 
- 
- var mediaConstraints = {
+
+
+console.log("SERVER");
+
+console.log("THE FIRST STEP IS ON THE CLIENT CONSOLE");
+
+var options = {
+    optional: [{
+        RtpDataChannels: true
+    }, {
+        DtlsSrtpKeyAgreement: true
+    }]
+};
+var iceServers;
+if (webrtcDetectedBrowser == "chrome") {
+    iceServers = {
+        "iceServers": createIceServer("stun:stun.l.google.com:19302")
+    };
+}
+else if (webrtcDetectedBrowser == "firefox") {
+    iceServers = {
+        "iceServers": createIceServer("stun:stun.services.mozilla.com")
+    };
+}
+
+
+var pc = new RTCPeerConnection(iceServers, options);
+pc.onicecandidate = onIceCandidate;
+pc.ondatachannel = onDataChannel;
+
+var channel;
+
+
+var mediaConstraints = {
     optional: [],
     mandatory: {
-        OfferToReceiveAudio: false, 
-        OfferToReceiveVideo: false 
+        OfferToReceiveAudio: false,
+        OfferToReceiveVideo: false
     }
 };
 
 function receiveOffer(offerSdp) {
-    
+
     pc.ondatachannel = onDataChannel;
     pc.setRemoteDescription(offerSdp);
     pc.createAnswer(onDescription, null, mediaConstraints);
 
 }
- 
- function onDataChannel(event) {     
-     channel = event.channel;
-     channel.onmessage = onMessage;
-     channel.onopen = onChannelStateChange;
-     channel.onclose = onChannelStateChange;    
- }
- 
- function setCandidate(candidateJson) {
-     var candidate = JSON.parse(candidateJson);
-     pc.addIceCandidate(candidate);
- }
- 
- var iceCandidates = [];
- function onIceCandidate(event) {     
-     if (event.candidate) {
-         iceCandidates.push(event.candidate);
-     }
- }
- 
- 
- function onChannelStateChange(event) {
-     
-     if (event.srcElement.readyState == "open") {
+
+function onDataChannel(event) {
+    channel = event.channel;
+    channel.onmessage = onMessage;
+    channel.onopen = onChannelStateChange;
+    channel.onclose = onChannelStateChange;
+}
+
+function setCandidate(candidateJson) {
+    var candidate = JSON.parse(candidateJson);
+    pc.addIceCandidate(candidate);
+}
+
+var iceCandidates = [];
+
+function onIceCandidate(event) {
+    if (event.candidate) {
+        iceCandidates.push(event.candidate);
+    }
+}
+
+
+function onChannelStateChange(event) {
+
+    if (event.srcElement.readyState == "open") {
         console.log("CONNECTION ESTABLISHED: now use channel.send('message') to send messages");
-     }
- }
- 
- function onDescription(desc) {
-     console.log("2 - Answer created. Send answer to client. Copy & Paste on the client console:");     
-     console.log('setRemoteDescription(new RTCSessionDescription(JSON.parse(\'' + JSON.stringify(desc).replace(/\\/g,"\\\\") + '\')));');
-     pc.setLocalDescription(desc);
- }
- 
- function setCandidates(candidates) {
-     for (var i = 0; i < candidates.length; i++) {
+    }
+}
+
+function onDescription(desc) {
+    console.log("2 - Answer created. Send answer to client. Copy & Paste on the client console:");
+    console.log('setRemoteDescription(new RTCSessionDescription(JSON.parse(\'' + JSON.stringify(desc).replace(/\\/g, "\\\\") + '\')));');
+    pc.setLocalDescription(desc);
+}
+
+function setCandidates(candidates) {
+    for (var i = 0; i < candidates.length; i++) {
         pc.addIceCandidate(new RTCIceCandidate(candidates[i]));
-     }
-     console.log("4 - SEND ice candidates to client. Copy & Paste on the client console:");
-     console.log("setCandidates(JSON.parse('" + JSON.stringify(iceCandidates).replace(/\\/g,"\\\\") + "'));")
- }
- 
- 
- 
- function close() {
-     console.log("close");
-     channel.close();
-     pc.close();
-     
- }
- 
-  function onMessage(event) {
-     console.log('Message received: ' + event.data);     
- }
+    }
+    console.log("4 - SEND ice candidates to client. Copy & Paste on the client console:");
+    console.log("setCandidates(JSON.parse('" + JSON.stringify(iceCandidates).replace(/\\/g, "\\\\") + "'));")
+}
+
+
+
+function close() {
+    console.log("close");
+    channel.close();
+    pc.close();
+
+}
+
+function onMessage(event) {
+    console.log('Message received: ' + event.data);
+}
